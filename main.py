c70f8407ba10303ee1bfa0c7afc37752a3749b9f
duplxey <nick.tomazic@gmail.com>
2025-11-20 16:01:48 +0100

    chat_complete_view(), chat_stream_view()

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-import asyncio
 import logging
 import os
 
 from dotenv import load_dotenv
 from fastapi import FastAPI, HTTPException, Request
-from openai import OpenAI
+from openai import AsyncOpenAI
 from starlette.responses import StreamingResponse
 from starlette.templating import Jinja2Templates
 
@@ -15,7 +14,7 @@ load_dotenv()
 # Initialize FastAPI, templates, and OpenAI client
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
-openai_client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+openai_client = AsyncOpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
 
 
 @app.get("/")
@@ -28,10 +27,28 @@ def index_view(request: Request):
     )
 
 
-@app.get("/stream")
-async def stream(message: str):
+@app.get("/chat-complete")
+async def chat_complete_view(message: str):
     try:
-        completion = openai_client.responses.create(
+        response = await openai_client.responses.create(
+            model="gpt-4o-mini",
+            input=message,
+        )
+        return {
+            "data": response.output_text,
+        }
+    except Exception as e:
+        logging.error(f"Error: {str(e)}")
+        raise HTTPException(
+            detail=str(e),
+            status_code=500,
+        )
+
+
+@app.get("/chat-stream")
+async def chat_stream_view(message: str):
+    try:
+        response = await openai_client.responses.create(
             model="gpt-4o-mini",
             input=message,
             stream=True,
@@ -41,14 +58,13 @@ async def stream(message: str):
         async def async_generator():
             yield "event: start\ndata: [START]\n\n"
 
-            for event in completion:
+            async for event in response:
                 if event.type == "response.output_text.delta":
                     text = event.delta
                     yield f"data: {text}\n\n"
                 if event.type == "response.completed":
                     total_tokens = event.response.usage.total_tokens
                     logging.info(f"Used tokens: {total_tokens}")
-                await asyncio.sleep(0)
 
             yield "event: end\ndata: [END]\n\n"
 
@@ -60,7 +76,6 @@ async def stream(message: str):
                 "X-Accel-Buffering": "no",
             },
         )
-
     except Exception as e:
         logging.error(f"Error: {str(e)}")
         raise HTTPException(
